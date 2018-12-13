540321e6738a8a4dd5c17e5b1617ac35dddca3dc
B. van Berkum <dev@dotmpe.com>
2018-12-13 02:01:13 +0100

    Testing new cl-jupyter build

--- _/cl-jupyter/env.sh
+++ _/cl-jupyter/env.sh
@@ -2,4 +2,24 @@
 set -e
 . $WORKSPACE/tools/build-env.sh
 
+
+case "$SOURCE_BRANCH" in
+
+  cl-jupyter-dev )
+      X_DCKR_BASE=bvberkum/treebox
+      X_DCKR_TAG=dev
+    ;;
+
+  #cl-jupyter/treebox-dev )
+  #    X_DCKR_BASE=bvberkum/treebox
+  #    X_DCKR_TAG=dev
+  #  ;;
+
+  cl-jupyter/baseimage-dev )
+      X_DCKR_BASE=phusion/baseimage
+      X_DCKR_TAG=0.10.0
+    ;;
+
+esac
+
 #DOCKER_TAGS=cl-jupyter

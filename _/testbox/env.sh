6c24319a2960170dd8146be3fd2f87da9ca8615b
B. van Berkum <dev@dotmpe.com>
2019-04-03 00:21:45 +0200

    New basebox, scripts [testbox]

--- _/testbox/env.sh
+++ _/testbox/env.sh
@@ -2,5 +2,19 @@
 set -e
 . $WORKSPACE/tools/build-env.sh
 
+case "$DOCKER_TAG" in
+
+  dev )
+      X_DCKR_BASETAG=dev
+
+      DOCKER_TAGS="$DOCKER_TAGS basebox-$X_DCKR_BASETAG baseimage-master"
+    ;;
+
+  * )
+      echo "No basetag for '$DOCKER_TAG'" >&2
+      exit 1
+    ;;
+
+esac
+
 VERSION=0.0.1-dev
-#DOCKER_TAGS=testbox

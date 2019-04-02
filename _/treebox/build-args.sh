129d0839f58bf6ccfbb8c4e6a33dfea303a223f8
B. van Berkum <dev@dotmpe.com>
2019-04-03 01:17:51 +0200

    New testbox, syncing scripts [treebox]

--- _/treebox/build-args.sh
+++ _/treebox/build-args.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # Use previous LTS for zmq
-node_version=8.14.0
+#node_version=8.14.0
+node_version=10.15.3
--- _/treebox/env.sh
+++ _/treebox/env.sh
@@ -2,5 +2,19 @@
 set -e
 . $WORKSPACE/tools/build-env.sh
 
+case "$DOCKER_TAG" in
+
+  dev )
+      X_DCKR_BASETAG=dev
+
+      DOCKER_TAGS="$DOCKER_TAGS testbox-dev basebox-dev baseimage-master"
+    ;;
+
+  * )
+      echo "No basetag for '$DOCKER_TAG'" >&2
+      exit 1
+    ;;
+
+esac
+
 VERSION=0.0.2-dev
-#DOCKER_TAGS=treebox

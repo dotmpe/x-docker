e43bc81bf1144d35c19189d557e2d385410ba3b9
B. van Berkum <dev@dotmpe.com>
2019-04-03 21:02:27 +0200

    GIT hooks fix

--- .githooks/pre-commit/015-x-docker-branch-readme.sh
+++ .githooks/pre-commit/015-x-docker-branch-readme.sh
@@ -19,13 +19,18 @@ test ! -e "ReadMe-$BRANCH_NAME.md" && {
     $LOG warn "" "README.md is not a symlink" "" 1
   }
 
-  test "$(readlink README.md)" = "ReadMe-$BRANCH_NAME.md" && {
-    $LOG pass "" "README--branch matches" "$BRANCH_NAME"
+  case "$BRANCH_NAME" in
+    *-dev ) devline=${BRANCH_NAME%-dev} ;;
+    * ) devline=${BRANCH_NAME} ;;
+  esac
+
+  test "$(readlink README.md)" = "ReadMe-$devline.md" && {
+    $LOG pass "" "README--branch matches" "$devline"
   } || {
 
-    $LOG warn "" "README--branch mismatch, fixxing" "$BRANCH_NAME"
+    $LOG warn "" "README--branch mismatch, fixxing" "$devline"
     # Auto-fix and force update README.md symlink
-    rm README.md && ln -s ReadMe-$BRANCH_NAME.md README.md
+    rm README.md && ln -s ReadMe-$devline.md README.md
     git add README.md
     $status 1
   }
